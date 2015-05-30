e1a6617cd998285e5ad5d6e17afc6b9eaba5042b
李向 <270467360@qq.com>
2015-05-30 12:21:22 +0800

    修改下拉刷新

--- tongle/src/com/sogou/contest/tongle/fragment/HomePagerF.java
+++ tongle/src/com/sogou/contest/tongle/fragment/HomePagerF.java
@@ -18,46 +18,24 @@ import android.widget.ListView;
 import com.sogou.contest.tongle.R;
 import com.sogou.contest.tongle.adapter.HomeAdapter;
 
-public class HomePagerF extends Fragment implements OnRefreshListener {
+public class HomePagerF extends Fragment {
 
 	private View view;
 	private ListView xListView;
 	private List<String> list;
 	private HomeAdapter adapter;
-	private SwipeRefreshLayout swipeRefreshLayout;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		view = inflater.inflate(R.layout.homepager, null);
 		xListView = (ListView) view.findViewById(R.id.mListView);
-		swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshLayout);
 		list = new ArrayList<String>();
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 20; i++) {
 			list.add(i+"");
 		}
 		adapter = new HomeAdapter(list, getActivity());
 		xListView.setAdapter(adapter);
 
-		// 设置卷内的颜色
-		swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright, android.R.color.holo_green_light, android.R.color.holo_orange_light,
-				android.R.color.holo_red_light);
-		// 设置下拉刷新监听
-		swipeRefreshLayout.setOnRefreshListener(this);
 		return view;
-	}
-
-
-	@Override
-	public void onRefresh() {
-		new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-
-			@Override
-			public void run() {
-				list.add(0, "添加新的item" + new Random().nextInt());
-				adapter.notifyDataSetChanged();
-				// 停止刷新动画
-				swipeRefreshLayout.setRefreshing(false);
-			}
-		}, 2000);
 	}
 }
